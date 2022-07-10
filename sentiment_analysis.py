71a51af698021c627d21d45f7d6766b25ec15c28
aravindan-jambunathan <109034906+aravindan-jambunathan@users.noreply.github.com>
2022-07-10 20:11:09 +0400

    init commit

---
+++ sentiment_analysis.py
@@ -0,0 +1,41 @@
+#Description
+import streamlit as st
+import sys
+sys.path.append('src/')
+import joblib 
+
+
+#Title
+st.write("""
+# SENTIMENT ANALYSIS
+""")
+
+__version__ = "3.2.3"
+
+#Get Feature input from users
+
+def get_user_text():
+    user_test = st.text_input('Enter text here: ', '')
+    return(user_test)
+    
+#Store the user inputs
+user_text = get_user_text()
+
+model = joblib.load('src/SVC(pipeline)')
+
+sentiment = model.predict([user_text])
+if sentiment == 0:
+    sentiment = 'Negative'
+elif sentiment == 1:
+    sentiment = 'Positive'
+st.write(sentiment)
+
+
+    
+
+import sys
+from streamlit import cli as stcli
+
+if __name__ == '__main__':
+    sys.argv = ["streamlit", "run", "webapp.py"]
+    sys.exit(stcli.main())
